d80146e6c505648ca1eb23ea50ffc3245c0b76b3
James Figler <jrfig626@gmail.com>
2016-08-09 20:36:51 -0400

    converts time worked to dollars earned

--- src/main/java/com/jamesfigler/kata/babysitter/Babysitter.java
+++ src/main/java/com/jamesfigler/kata/babysitter/Babysitter.java
@@ -1,7 +1,13 @@
 package com.jamesfigler.kata.babysitter;
 
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 public class Babysitter {
-    public int calculate(String startTime, String endTime) {
-        return 12;
+    public int calculate(String startTime, String endTime) throws Exception {
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm aa");
+        long difference = formatter.parse(endTime).getTime() - formatter.parse(startTime).getTime();
+        long hours = TimeUnit.MILLISECONDS.toHours(difference);
+        return (int) (hours * 12);
     }
 }
--- src/test/java/com/jamesfigler/kata/babysitter/BabysitterTest.java
+++ src/test/java/com/jamesfigler/kata/babysitter/BabysitterTest.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.assertThat;
 public class BabysitterTest {
 
     @Test
-    public void shouldGetPaidTwelveDollarsPerHourFromStartToBedtime() {
+    public void shouldGetPaidTwelveDollarsPerHourFromStartToBedtime() throws Exception {
         Babysitter underTest = new Babysitter();
         int result = underTest.calculate("6:00 PM", "7:00 PM");
         assertThat(result, is(12));
+    }
+
+    @Test
+    public void shouldGetPaidTwentyFourDollarsForTwoHoursBeforeBedtime() throws Exception {
+        Babysitter underTest = new Babysitter();
+        int result = underTest.calculate("5:00 PM", "7:00 PM");
+        assertThat(result, is(24));
     }
 }
