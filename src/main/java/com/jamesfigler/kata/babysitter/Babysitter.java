a35669cbf9618ed86b50eb96c86c341d3651a2c1
James Figler <jrfig626@gmail.com>
2016-08-09 23:27:38 -0400

    extracted time conversion to its own method

--- src/main/java/com/jamesfigler/kata/babysitter/Babysitter.java
+++ src/main/java/com/jamesfigler/kata/babysitter/Babysitter.java
@@ -7,24 +7,11 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class Babysitter {
 
     public int calculate(String startTime, String endTime, String bedTime) throws Exception {
-        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm aa");
 
-        int totalPay = 0;
-        int dayInMilliseconds = 24 * 60 * 60 * 1000;
-
-        long start = formatter.parse(startTime).getTime();
-        long end = formatter.parse(endTime).getTime();
-        long bed = formatter.parse(bedTime).getTime();
-        long midnight = formatter.parse("12:00 AM").getTime() + dayInMilliseconds;
-
-        if(startTime.endsWith("AM")) {
-            // advance to next day if AM
-            start += dayInMilliseconds;
-        }
-        if(endTime.endsWith("AM")) {
-            // advance to next day if AM
-            end += dayInMilliseconds;
-        }
+        long start = convertTimeToMilliseconds(startTime);
+        long end = convertTimeToMilliseconds(endTime);
+        long bed = convertTimeToMilliseconds(bedTime);
+        long midnight = convertTimeToMilliseconds("12:00 AM");
 
         if(bed > end) {
             return (int) (12 * MILLISECONDS.toHours(end - start));
@@ -41,9 +28,22 @@ public class Babysitter {
         long hoursBeforeBedtime = MILLISECONDS.toHours(bed - start);
         long hoursAfterBedtime = MILLISECONDS.toHours(end - bed);
 
+        int totalPay = 0;
         totalPay += 12 * hoursBeforeBedtime;
         totalPay += 8 * hoursAfterBedtime;
 
         return totalPay;
+    }
+
+    private long convertTimeToMilliseconds(String time) throws Exception {
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm aa");
+        long output = formatter.parse(time).getTime();
+
+        // advance to next day if time is AM
+        if(time.endsWith("AM")) {
+            output += 24 * 60 * 60 * 1000;
+        }
+
+        return output;
     }
 }
